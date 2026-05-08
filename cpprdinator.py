144c6d19eace6eaa9aa1d10366e2f044cca3eca6
Ivan <optimusprimeodiseo01@gmail.com>
2026-05-08 05:43:25 -0300

    Create cpprdinator.py
    // Import the functions you need from the SDKs you need
    import { initializeApp } from "firebase/app";
    // TODO: Add SDKs for Firebase products that you want to use
    // https://firebase.google.com/docs/web/setup#available-libraries
    
    // Your web app's Firebase configuration
    const firebaseConfig = {
      apiKey: "AIzaSyCX4Hnq9kE6D8SfVS7gGZUp6xehQYp8-hg",
      authDomain: "studio-8526301920-20b7e.firebaseapp.com",
      projectId: "studio-8526301920-20b7e",
      storageBucket: "studio-8526301920-20b7e.firebasestorage.app",
      messagingSenderId: "431292081671",
      appId: "1:431292081671:web:f76b33d86984d75b842ca2"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);

---
+++ cpprdinator.py
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCX4Hnq9kE6D8SfVS7gGZUp6xehQYp8-hg",
+  authDomain: "studio-8526301920-20b7e.firebaseapp.com",
+  projectId: "studio-8526301920-20b7e",
+  storageBucket: "studio-8526301920-20b7e.firebasestorage.app",
+  messagingSenderId: "431292081671",
+  appId: "1:431292081671:web:f76b33d86984d75b842ca2"
+};
